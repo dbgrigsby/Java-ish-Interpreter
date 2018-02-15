6f242926fe017c52a8e2dc32401aeae868c04a5f
Daniel Grigsby <dbg28@case.edu>
2018-02-14 20:55:21 -0500

    Remove extra comments

--- tester.scm
+++ tester.scm
@@ -8,35 +8,35 @@
         (string-append "Failed " filename ". Output was " (interpret filename) ", but should have been " expected-output))))
 
 ; Tests 1
-(test "Tests/test1.java" 150) ;A has main
-(test "Tests/test2.java" -4) ;A has main
-(test "Tests/test3.java" 10) ;A has main
-(test "Tests/test4.java" 16) ;A has main
-(test "Tests/test5.java" 220) ;A has main
-(test "Tests/test6.java" 5) ;A has main
-(test "Tests/test7.java" 6) ;C has main
-(test "Tests/test8.java" 10) ;square has main
-(test "Tests/test9.java" 9) ;square has main
-(test "Tests/test10.java" -39) ;List has main
-(test "Tests/test11.java" `error) ;List has main
-(test "Tests/test8.java" 10) ;square has main
-(test "Tests/test9.java" 9) ;square has main
-(test "Tests/test10.java" -39) ;List has main
-(test "Tests/test11.java" `error) ;List has main
-(test "Tests/test12.java" `error) ;List has main
-(test "Tests/test13.java" `error) ;List has main
-(test "Tests/test14.java" `error) ;List has main
-(test "Tests/test15.java" `true) ;A has main
-(test "Tests/test16.java" 100) ;A has main
-(test "Tests/test17.java" `false`) ;A has main
-(test "Tests/test18.java" true) ;A has main
-(test "Tests/test19.java" 128) ;A has main
-(test "Tests/test20.java" 12) ;A has main
-(test "Tests/test21.java" 30) ;C has main
-(test "Tests/test22.java" 11) ;square has main
-(test "Tests/test23.java" 1106) ;square has main
-(test "Tests/test24.java" 12) ;List has main
-(test "Tests/test25.java" 16) ;List has main
-(test "Tests/test26.java" 72) ;square has main
-(test "Tests/test27.java" 21) ;square has main
-(test "Tests/test28.java" 164) ;List has main
+(test "Tests/test1.java" 150)
+(test "Tests/test2.java" -4)
+(test "Tests/test3.java" 10)
+(test "Tests/test4.java" 16)
+(test "Tests/test5.java" 220)
+(test "Tests/test6.java" 5)
+(test "Tests/test7.java" 6)
+(test "Tests/test8.java" 10)
+(test "Tests/test9.java" 9)
+(test "Tests/test10.java" -39)
+(test "Tests/test11.java" `error)
+(test "Tests/test8.java" 10)
+(test "Tests/test9.java" 9)
+(test "Tests/test10.java" -39)
+(test "Tests/test11.java" `error)
+(test "Tests/test12.java" `error)
+(test "Tests/test13.java" `error)
+(test "Tests/test14.java" `error)
+(test "Tests/test15.java" `true)
+(test "Tests/test16.java" 100)
+(test "Tests/test17.java" `false`)
+(test "Tests/test18.java" true)
+(test "Tests/test19.java" 128)
+(test "Tests/test20.java" 12)
+(test "Tests/test21.java" 30)
+(test "Tests/test22.java" 11)
+(test "Tests/test23.java" 1106) 
+(test "Tests/test24.java" 12)
+(test "Tests/test25.java" 16)
+(test "Tests/test26.java" 72)
+(test "Tests/test27.java" 21)
+(test "Tests/test28.java" 164)
