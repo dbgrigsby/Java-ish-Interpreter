aa13169f0c075f71582213bedaf129038da99288
Adam Beck <akb93@case.edu>
2018-03-08 20:26:28 -0500

    Organize helpers chronologically

--- helpers.scm
+++ helpers.scm
@@ -4,98 +4,85 @@
 #lang racket
 (provide (all-defined-out))
 
-
+; Logic of the scoped-state structure
 (define initstate '( (() ()) ))
 (define nullreturn '())
 (define get-top-scope car)
 (define get-tail-scope cdr)
 
 
-; Important section helper functions for abstraction are defined below
+
+; State logic of of a state in the scoped-state structure
+(define get-state-from-pair cadr)
+(define get-value-from-pair car)
+(define get-variable-section-state car)
+(define get-value-section-state cadr)
+(define get-variable-section-head car)
+(define get-variable-section-tail cdr)
+(define get-state-variable-head caar)
+(define get-state-variable-tail cdar)
+(define get-state-value-head caadr)
+(define get-state-value-tail cdadr)
+
+
+
+; Structure of the program arguments section
+; Gets the first argument of the list of arguments
 (define program-head car)
 (define program-tail cdr)
+(define arglist-head car)
+(define arglist-tail cdr)
+(define get-arg1-from-expr cadr)
+(define get-arg2-from-expr caddr)
 
 
+
+; Individual argument section
 ; Returns the type of the upcoming statement in an arglist
 ; (e.g. (var x (+ 1 2)) yields 'var)
 (define get-upcoming-statement-name car)
 
 
-; Important section helper functions for abstraction are defined below
-(define rest-of-return-statement cdr)
-(define rest-of-begin-statement cdr)
 
-
-
-
-; Important section helper functions for abstraction are defined below
-(define get-if-cond cadr)
-(define get-if-then caddr)
-
-
+; Assign/Declare statement section
+(define get-var-name-from-declare-args cadr)
+(define truncate-var-name-from-declare cddr)
 (define get-declare-from-assign cddr)
 
 
 
-
-(define inner-argument caar)
-
-
-
-(define get-inner-catch-statement cdar)
-(define get-inner-finally-statement cdar)
-(define get-statements-from-try cadr)
-
-(define get-catch-wrapper caddr)
-
-(define get-contents-of-catch cddr)
-
-(define get-finally-wrapper cadddr)
-
-(define get-contents-of-finally cdddr)
-
-(define get-contents-of-throw cadr)
+; Return statement section
+(define rest-of-return-statement cdr)
 
 
 
-; Important section helper functions for abstraction are defined below
-(define get-while-cond cadr)
-(define get-while-statement caddr)
+; Begin statement section
+(define rest-of-begin-statement cdr)
 
 
-; Important section helper functions for abstraction are defined below
-(define get-var-name-from-declare-args cadr)
-(define truncate-var-name-from-declare cddr)
 
+; If statement section
+(define get-if-cond cadr)
+(define get-if-then caddr)
 (define get-args-after-if-else cadddr)
 (define get-else-from-if-else cdddr)
 
 
 
+; While statement seciton
+(define get-while-cond cadr)
+(define get-while-statement caddr)
 
 
-; Important section helper functions for abstraction are defined below
-(define arglist-head car)
-(define arglist-tail cdr)
 
-
-(define get-arg1-from-expr cadr)
-(define get-arg2-from-expr caddr)
-(define get-state-from-pair cadr)
-(define get-value-from-pair car)
-
-(define get-variable-section-head car)
-(define get-variable-section-tail cdr)
-
+; Try/catch/finally statement section
+(define get-inner-catch-statement cdar)
+(define get-inner-finally-statement cdar)
+(define get-statements-from-try cadr)
+(define get-catch-wrapper caddr)
+(define get-contents-of-catch cddr)
+(define get-finally-wrapper cadddr)
+(define get-contents-of-finally cdddr)
+(define get-contents-of-throw cadr)
 (define get-exception-from-catch caar)
 (define get-statements-from-catch cadr)
-
-; The state is stored as a list of two lists
-; (e.g. the head of the values for '((a b c) (1 2 3)) is 1, by calling caadr)
-; (e.g. the tail of the variables for '((a b c) (1 2 3)) is (b c), by calling cdar)
-(define get-state-variable-head caar)
-(define get-state-variable-tail cdar)
-(define get-state-value-head caadr)
-(define get-state-value-tail cdadr)
-(define get-variable-section-state car)
-(define get-value-section-state cadr)
