fc6d2863818a485ea33ab0d38c0d113ef6049454
Adam Beck <akb93@case.edu>
2018-04-01 02:17:21 -0400

    Test 0 example

--- Tests/3/test0.java
+++ Tests/3/test0.java
@@ -1,11 +1,7 @@
 function test(a) {
-  return test2(a) + 1;
-}
-function test2(b) {
- return b + 1;
-}
-function main() {
-  var x = test(3);
-  return test(x);
+return 2;
 }
 
+function main() {
+  return test(1);
+}
