711447728eb3bb2aa9f4047e3564735698c12a85
Brett Johnson <btj12@case.edu>
2018-04-02 17:17:39 -0400

    Test 0

--- Tests/3/test0.java
+++ Tests/3/test0.java
@@ -1,18 +1,30 @@
-function divide(y) {
-  throw y;
+function divide(x, y) {
+  if (y == 0)
+    throw 1000000;
+  return x / y;
 }
 
-function main() {
-  var x = 5;
+ function main() {
+  var x = 0;
+  var j = 1;
 
   try {
-    x = divide(5);
+    while (j >= 0) {
+    var i = 100;
+    while (i >= 0) {
+      try {
+        x = x + divide(10*i, i);
+      }
+      catch(e) {
+        x = x + divide(e, j);
+      }
+      i = i - 1;
+    }
+    j = j - 1;
+   }
   }
-  catch(e) {
-    x = e;
-  }
-  finally {
-    x = x + 100;
+  catch (e2) {
+    x = x * 2;
   }
   return x;
 }
